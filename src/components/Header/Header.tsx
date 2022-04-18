3448f0c52908037ca0fdc15d4e13c54a30713cbe
Tom Fraczek <tomfraczekdev@gmail.com>
2022-04-18 13:53:17 +0200

    boilertplate - first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,5 @@
+import { StyledHeader } from '../styles/Header.styled';
+
+const Header = () => <StyledHeader>SDH Frontedn Homework</StyledHeader>;
+
+export default Header;
---
+++ src/components/styles/Header.styled.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.header`
+  background: none;
+  height: 60px;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  border-bottom: 1px solid #000;
+`;
